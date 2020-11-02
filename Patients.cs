6c01f09a77fa5e98239faab70949cbae424172f1
Abah Joseph Israel <42052027+abahjosephisrael@users.noreply.github.com>
2020-11-02 03:23:47 +0100

    Add files via upload

---
+++ Patients.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital_Management_System
+{
+    public class Patient
+    {
+        public string Name { get; set; }        
+        public string Blood_Group { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public string Treatment { get; set; }
+        public string Address { get; set; }
+        public long Phone { get; set; }
+        public string Bill { get; set; }
+        public string Next_Of_Kin { get; set; }                
+        public long Next_Of_Kin_Phone { get; set; }
+        public string Next_Of_Kin_Address { get; set; }
+
+    }
+}
---
+++ frmProgress.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Hospital_Management_System
+{
+    partial class frmProgress
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // progressBar
+            // 
+            this.progressBar.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.progressBar.ForeColor = System.Drawing.Color.Green;
+            this.progressBar.Location = new System.Drawing.Point(0, 0);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(718, 38);
+            this.progressBar.TabIndex = 0;
+            this.progressBar.UseWaitCursor = true;
+            // 
+            // timer
+            // 
+            this.timer.Interval = 2000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // frmProgress
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(718, 39);
+            this.Controls.Add(this.progressBar);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Movable = false;
+            this.Name = "frmProgress";
+            this.Resizable = false;
+            this.ShadowType = MetroFramework.Forms.MetroForm.MetroFormShadowType.DropShadow;
+            this.ShowIcon = false;
+            this.UseWaitCursor = true;
+            this.Load += new System.EventHandler(this.frmProgress_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Timer timer;
+    }
+}
